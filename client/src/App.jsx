777ea50d8fa14cc762a83b5106f0793636eff482
William Gay <williamgay360@gmail.com>
2025-04-16 11:42:47 -0400

    fixing url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   // Local Development
   // const BACKEND_URL = 'http://0.0.0.0:8000'
-  const BACKEND_URL = 'https://opto-demo-production.up.railway.app/'
+  const BACKEND_URL = 'https://opto-demo-production.up.railway.app'
 
   useEffect(() => {
     fetch(`${BACKEND_URL}/`)
